bacb9153f371aad36a7f00a17d9a96b95aebd1a3
xbisharax <bishara.said@gmail.com>
2017-04-10 12:00:53 -0400

    Update inventory.cpp

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,14 +1,18 @@
 //inventory.cpp
 
-#include <inventory.h>
+#include "inventory.h"
 #include "book.h"
+//needed for the std::sort to work here
+#include <algorithm>
 
 //#include <vector>
 
+//may not need these but we'll see
 //constructor
 inventory::inventory(){}
 //deconstructor
 inventory::~inventory(){}
+
 
 bool inventory::TitleCompare(book i, book j){
     return (i.getName() > j.getName());
@@ -19,9 +23,10 @@ void inventory::addBook(book obj){
     //POST: a book will be added to the list of books
     books.push_back(obj);
     //sort the list using the std sort
-    std::sort(books.begin(), books.end(), TitleCompare());
+    std::sort(books.begin(), books.end(), TitleCompare);
     }
 
+//used to add new books
 bool inventory::orderBook(string title){
     //PRE: none
     //POST: the list size will be incremented and a new book will be in stock
@@ -37,10 +42,12 @@ bool inventory::orderBook(string title){
     return found;
     }
 
+//similar to "checking out a book", not removing current book
 void inventory::sellStock(string title){
     //PRE: the book must be available and not out of stock
-    //POST: the book removed must also be removed from the list of books available
+    //POST: decreasing the stock of the book by 1
     
+    //call the lowOnHandAlert after decrementing the stock of the book
     }
 
 //prompt
@@ -56,6 +63,7 @@ void inventory::lowOnHandAlert(int index){
         }
     }
 
+//displays the current inventory
 void inventory::display(){
     //PRE:
     //POST:
